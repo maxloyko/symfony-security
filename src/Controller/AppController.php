e37d98beb2b16e341e1f635a01af9f967262e32c
Max Loyko <maxloyko@gmail.com>
2023-01-06 01:26:31 -0500

    add register/pass validation

---
+++ src/Controller/AppController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function index()
+    {
+        return $this->render('app/index.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
+
+    #[Route('/not-the-home-page', name: 'alternate-page')]
+    public function alternatePage()
+    {
+        return $this->render('app/alternate-page.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
+}
